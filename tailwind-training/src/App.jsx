a5df5959a9397c4a1f4d491d3472af91cb310b4e
Kisthanny <tigerqige@gmail.com>
2024-04-01 11:59:58 +0800

    componentnize all of our sections

--- tailwind-training/src/App.jsx
+++ tailwind-training/src/App.jsx
@@ -1,15 +1,42 @@
+import Nav from "./components/Nav";
+import {
+  Hero,
+  PopularProducts,
+  SuperQuality,
+  Services,
+  SpecialOffer,
+  CustomerReview,
+  Subscribe,
+  Footer,
+} from "./sections/index";
 export default function App() {
   return (
     <main className="relative">
-      Nav
-      <section className="xl:padding-l wide:padding-r padding-b">Hero</section>
-      <section className="padding">PopularProducts</section>
-      <section className="padding">SuperQuality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">SpecialOffer</section>
-      <section className="padding bg-pale-blue">CustomerReview</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className="padding bg-black padding-x  padding-t padding-b">Footer</section>
+      <Nav></Nav>
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffer />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReview />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="padding bg-black padding-x  padding-t padding-b">
+        <Footer />
+      </section>
     </main>
   );
 }
