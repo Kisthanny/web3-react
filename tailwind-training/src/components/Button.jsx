22fba87cd57b06e79ff999ca1f835cac13fc0615
Kisthanny <tigerqige@gmail.com>
2024-04-01 20:40:15 +0800

    SuperQuality section complete

--- tailwind-training/src/components/Button.jsx
+++ tailwind-training/src/components/Button.jsx
@@ -1,8 +1,10 @@
-const Button = ({label, iconUrl}) => {
+const Button = ({ label, iconUrl }) => {
   return (
     <button className="rounded-full flex items-center justify-center gap-4 px-7 py-4 bg-coral-red hover:bg-red-500 mt-4">
-      <span className="text-white text-lg font-montserrat leading-none">{label}</span>
-      <img className="rounded-full w-5 h-5" src={iconUrl} alt="" />
+      <span className="text-white text-lg font-montserrat leading-none">
+        {label}
+      </span>
+      {iconUrl && <img className="rounded-full w-5 h-5" src={iconUrl} alt="" />}
     </button>
   );
 };
--- tailwind-training/src/sections/SuperQuality.jsx
+++ tailwind-training/src/sections/SuperQuality.jsx
@@ -1,5 +1,31 @@
+import { shoe8 } from "../assets/images";
+import Button from "../components/Button";
 const SuperQuality = () => {
-    return ( <div>SuperQuality</div> );
-}
- 
+  return (
+    <section className="flex lg:flex-row flex-col items-center gap-10 max-container">
+      <div className="flex flex-1 flex-col lg:max-w-lg">
+        <h2 className="text-4xl font-palanquin font-bold">
+          We Provide You <span className="text-coral-red">Super Quality</span>{" "}
+          Shoes
+        </h2>
+        <p className="mt-6 text-slate-gray text-lg font-montserrat info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elevate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance.
+          <br />
+          <span className="inline-block mt-4">
+            Our dedication to detail and excellence ensures your satisfaction
+          </span>
+        </p>
+        <div className="mt-4">
+          <Button label="View details" />
+        </div>
+      </div>
+      <div className="flex-1 flex items-center justify-center">
+        <img src={shoe8} alt="" className="object-contain" />
+      </div>
+    </section>
+  );
+};
+
 export default SuperQuality;
