6eecb10321563ed555d9e39df96e097d8a527aa3
Kisthanny <tigerqige@gmail.com>
2024-04-02 12:52:26 +0800

    specialOffer section complete

--- tailwind-training/src/components/Button.jsx
+++ tailwind-training/src/components/Button.jsx
@@ -1,7 +1,18 @@
-const Button = ({ label, iconUrl }) => {
+const Button = ({ label, iconUrl, grey }) => {
+  console.log({ label, grey });
   return (
-    <button className="rounded-full flex items-center justify-center gap-4 px-7 py-4 bg-coral-red hover:bg-red-500 mt-4">
-      <span className="text-white text-lg font-montserrat leading-none">
+    <button
+      className={`${
+        grey
+          ? "bg-white border-slate-gray border "
+          : "bg-coral-red hover:bg-red-500 "
+      }rounded-full flex items-center justify-center gap-4 px-7 py-4 mt-4`}
+    >
+      <span
+        className={`${
+          grey ? "text-slate-gray " : "text-white "
+        }text-lg font-montserrat leading-none text-nowrap`}
+      >
         {label}
       </span>
       {iconUrl && <img className="rounded-full w-5 h-5" src={iconUrl} alt="" />}
--- tailwind-training/src/sections/SpecialOffer.jsx
+++ tailwind-training/src/sections/SpecialOffer.jsx
@@ -1,5 +1,33 @@
+import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
+import Button from "../components/Button";
 const SpecialOffer = () => {
-    return ( <div>SpecialOffer</div> );
-}
- 
+  return (
+    <section className="max-container flex items-center justify-between gap-10 flex-col xl:flex-row-reverse">
+      <div className="flex-1 flex flex-col items-start">
+        <h2 className="text-4xl font-palanquin font-bold">
+          <span className="text-coral-red">Special</span> Offer
+        </h2>
+        <p className="info-text mt-4">
+          Embark on a shopping journey that redefines your experience with
+          unbeatable deals. From premier selections to incredible savings, we
+          offer unparalleled value that sets us apart.
+        </p>
+        <p className="info-text mt-6">
+          Navigate a realm of possibilities designed to fulfill your unique
+          desires, surpassing the loftiest expectations. Your journey with us is
+          nothing short of exceptional.
+        </p>
+        <div className="flex items-center justify-center gap-4 mt-7">
+          <Button label="Shop now" iconUrl={arrowRight} />
+          <Button label="Learn more" grey />
+        </div>
+      </div>
+      <div className="flex-1">
+        <img src={offer} alt="special offer" />
+      </div>
+    </section>
+  );
+};
+
 export default SpecialOffer;
